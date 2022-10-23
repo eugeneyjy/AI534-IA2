57e64fcc5ab46fa1c86edef4a3e400e892d033a7
Eugene Yong <yonge@oregonstate.edu>
2022-10-22 20:31:22 -0700

    Implemented logreg with l1 regularization

--- IA2_skeleton_code.py
+++ IA2_skeleton_code.py
@@ -29,9 +29,12 @@ def preprocess_data(data):
 def sigmoid(input):
     return 1 / (1 + np.exp(-input))
 
-def negative_loglikelihood(X, y, w, lambd):
+def negative_loglikelihood(X, y, w, lambd, l2=True):
     size = X.shape[0]
-    reg = lambd*(np.square(w[1:]).sum())
+    if l2:
+        reg = lambd*(np.square(w[1:]).sum())
+    else:
+        reg = lambd*(np.absolute(w[1:]).sum())
     sig = sigmoid(X@w)
     lw = (-y.T@np.log(sig) - (1-y).T@np.log(1-sig))/size
     return (lw + reg)[0][0]
@@ -81,9 +84,32 @@ def LR_L2_train(train_data, val_data, lr, lambd, max_epoch):
 # Trains a logistic regression model with L1 regularization on the provided train_data, using the supplied lambd
 # weights should store the per-feature weights of the learned logisitic regression model. train_acc and val_acc 
 # should store the training and validation accuracy respectively. 
-def LR_L1_train(train_data, val_data, lambd):
+def LR_L1_train(train_data, val_data, lr, lambd, max_epoch):
     # Your code here:
-
+    y_train = train_data.iloc[:, -1].to_numpy()[:, np.newaxis]
+    X_train = train_data.iloc[:, :-1].to_numpy()
+    y_val = val_data.iloc[:, -1].to_numpy()[:, np.newaxis]
+    X_val = val_data.iloc[:, :-1].to_numpy()
+    size = X_train.shape[0]
+    dims = X_train.shape[1]
+    weights = np.random.rand(dims, 1)
+    losses = []
+    # epsilon = 10e-5
+    for epoch in range(max_epoch):
+        gradients = (1/size)*((sigmoid(X_train@weights)-y_train).T@X_train).T
+        weights -= lr*gradients
+        penalty = np.sign(weights[1:])*np.maximum(np.absolute(weights[1:])-(lr*lambd), 0)
+        weights[1:] = penalty
+        grad_norm = np.linalg.norm(gradients) + np.linalg.norm(lr*lambd)
+        loss = negative_loglikelihood(X_train, y_train, weights, lambd, l2=False)
+        losses.append(loss)
+        # Stop training if loss starts to go up
+        if epoch > 0 and loss > losses[-2]:
+            break
+        print(f"lambda: {lambd}, epoch: {epoch}, loss: {loss}, gradient_norm: {grad_norm}")
+    train_acc = calc_accuracy(X_train, y_train, weights)
+    val_acc = calc_accuracy(X_val, y_val, weights)
+    print(f"train_acc: {train_acc}, val_acc: {val_acc}")
     return weights, train_acc, val_acc
 
 # Generates and saves plots of the accuracy curves. Note that you can interpret accs as a matrix
@@ -135,7 +161,7 @@ norm_train, norm_val = preprocess_data((train_data, val_data))
 
 # Part 1 . Implement logistic regression with L2 regularization and experiment with different lambdas
 # Your code here:
-# lambds = [0, 1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2]
+# lambds = [1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2]
 # lrs = [2.5, 2.5, 2, 2, 1, 0.01, 0.01, 0.01]
 # accs = []
 # all_weights = []
@@ -154,27 +180,42 @@ norm_train, norm_val = preprocess_data((train_data, val_data))
 
 # Part 2  Training and experimenting with IA2-train-noisy data.
 # Your code here:
-train_noisy_data = load_data('IA2-train-noisy.csv')
-norm_train_noisy, norm_val_noisy = preprocess_data((train_noisy_data, val_data))
-lambds = [0, 1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2]
-lrs_noisy = [2.5, 2.5, 2, 2, 1, 0.01, 0.01, 0.01]
-accs_noisy = []
-all_weights_noisy = []
+# train_noisy_data = load_data('IA2-train-noisy.csv')
+# norm_train_noisy, norm_val_noisy = preprocess_data((train_noisy_data, val_data))
+# lambds = [1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2]
+# lrs_noisy = [2.5, 2.5, 2, 2, 1, 0.01, 0.01, 0.01]
+# accs_noisy = []
+# all_weights_noisy = []
 
-for i in range(len(lambds)):
-    max_epoch = 10
-    weights, train_acc, val_acc = LR_L2_train(norm_train_noisy, norm_val_noisy, lrs_noisy[i], lambds[i], max_epoch)
-    accs_noisy.append([lambds[i], train_acc, val_acc])
-    all_weights_noisy.append(weights.flatten())
+# for i in range(len(lambds)):
+#     max_epoch = 10
+#     weights, train_acc, val_acc = LR_L2_train(norm_train_noisy, norm_val_noisy, lrs_noisy[i], lambds[i], max_epoch)
+#     accs_noisy.append([lambds[i], train_acc, val_acc])
+#     all_weights_noisy.append(weights.flatten())
 
-accs_noisy = np.array(accs_noisy)
-plot_losses(accs_noisy, "accuracies_p2.png", "accuracies_p2.csv")
-save_weights(norm_train.columns[:-1], all_weights_noisy, "weights_p2.csv")
-save_top_5_features(norm_train.columns[:-1], all_weights_noisy, lambds, "top_features_p2.csv")
-plot_sparsity(all_weights_noisy, lambds, "sparcities_p2.csv")
+# accs_noisy = np.array(accs_noisy)
+# plot_losses(accs_noisy, "accuracies_p2.png", "accuracies_p2.csv")
+# save_weights(norm_train.columns[:-1], all_weights_noisy, "weights_p2.csv")
+# save_top_5_features(norm_train.columns[:-1], all_weights_noisy, lambds, "top_features_p2.csv")
+# plot_sparsity(all_weights_noisy, lambds, "sparcities_p2.csv")
 
 # Part 3  Implement logistic regression with L1 regularization and experiment with different lambdas
 # Your code here:
+lambds = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e1, 1e2]
+lrs_l1 = [2.5, 2.5, 2, 2, 0.5, 0.05, 0.03, 0.01]
+accs_l1 = []
+all_weights_l1 = []
 
+for i in range(len(lambds)):
+    max_epoch = 10000
+    weights, train_acc, val_acc = LR_L1_train(norm_train, norm_val, lrs_l1[i], lambds[i], max_epoch)
+    accs_l1.append([lambds[i], train_acc, val_acc])
+    all_weights_l1.append(weights.flatten())
+
+accs_l1 = np.array(accs_l1)
+plot_losses(accs_l1, "accuracies_p3.png", "accuracies_p3.csv")
+save_weights(norm_train.columns[:-1], all_weights_l1, "weights_p3.csv")
+save_top_5_features(norm_train.columns[:-1], all_weights_l1, lambds, "top_features_p3.csv")
+plot_sparsity(all_weights_l1, lambds, "sparcities_p3.csv")
 
 
